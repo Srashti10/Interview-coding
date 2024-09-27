f946d2a9bdf75d7649324831839d741d56c28d0c
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-09-27 13:54:20 +0530

    changes

---
+++ swap-variable.py
@@ -0,0 +1,18 @@
+a = int(input('Enter the value of a:'))
+b = int(input('Enter the value of b:'))
+a,b = b,a
+print(a,b)
+
+aa = "Hello"
+bb = "World"
+aa,bb = bb,aa
+print(aa,bb)
+
+
+a = "10"
+b = "20"
+a = a+b
+b = a-b
+a = a-b
+print(a)
+print(b)
