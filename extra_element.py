d879f3595cba5d44ccb1723d3d911c192ec525ef
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-10-01 18:55:30 +0530

    changes

--- extra_element.py
+++ extra_element.py
@@ -1,8 +1,11 @@
 arr1 = [2,4,6,8,9,10,12]
 arr2 = [2,4,6,8,10,12]
-n1 = len(arr1)
-n2 = len(arr2)
-for i in (0,n1-1):
-    for j in (0,n2-1):
-        if arr1[i] != arr2[j]:
-            print(i)
+sum1 = 0
+sum2 = 0 
+for i in arr1:
+    sum1 = sum1 + i
+
+for j in arr2:
+    sum2 = sum2 + j
+
+print(sum1 - sum2)
