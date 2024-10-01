604ec5a102e8ffb83d244731a6a148a4201a62b1
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-10-01 21:06:31 +0530

    changes

---
+++ arr_sum.py
@@ -0,0 +1,5 @@
+arr = [10, 5, 78, 98, 457]
+sum = 0 
+for i in range(0, len(arr)):
+    sum = sum + arr[i]
+print(sum)
--- second_largest.py
+++ second_largest.py
@@ -1,4 +1,11 @@
-# arr = [12, 35, 1, 10, 34, 1]
+arr = [12, 35, 1, 10, 34, 1]
 # arr.sort()
 # print(arr[len(arr)-2])
-arr =[10,10]
+# arr =[10,10]
+new_arr = list(set(arr))
+new_arr.sort()
+n = len(new_arr)
+if new_arr[n-1] != new_arr[n-2]:
+    print(new_arr[n-2])
+else:
+    print(-1)
