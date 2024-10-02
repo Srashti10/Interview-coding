27ac83f09d90f6eb5c6503a74c256a909eb29668
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-10-02 18:00:43 +0530

    changes

--- factorial.py
+++ factorial.py
@@ -1,11 +1,11 @@
-def factorial(num):
-    if num == 0:
-        return 1
-    else:
-        return num * factorial(num-1)
+# def factorial(num):
+#     if num == 0:
+#         return 1
+#     else:
+#         return num * factorial(num-1)
     
-num = int(input('Enter the number: '))
-print(f'Factorial of {num} is ', factorial(num))
+# num = int(input('Enter the number: '))
+# print(f'Factorial of {num} is ', factorial(num))
 
 def factorial(num):
     if num == 0:
