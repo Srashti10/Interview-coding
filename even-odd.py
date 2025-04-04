c5bd80fdbb16aca7e55881287d724a676b94718e
Srashti Srivastava <srivastavasrashti45@gmail.com>
2025-04-04 13:30:31 +0530

    changes

--- even-odd.py
+++ even-odd.py
@@ -1,7 +1,17 @@
+# def even_odd(num):
+#     if num%2 == 0:
+#         return "Even Number"
+#     else:
+#         return "Odd Number"
+    
+# print(even_odd(5679))
+
+
+
 def even_odd(num):
     if num%2 == 0:
-        return "Even Number"
+        return ("Even Number")
     else:
-        return "Odd Number"
-    
-print(even_odd(5679))
+        return("Odd Number")
+
+print(even_odd(3456))
