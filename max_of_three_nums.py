8b2d7b1c29ffeb2e483c859a9822e3404ee92fb6
Srashti Srivastava <srivastavasrashti45@gmail.com>
2025-04-04 14:23:48 +0530

    changes

--- max_of_three_nums.py
+++ max_of_three_nums.py
@@ -1,13 +1,26 @@
+# def maximum(a,b,c):
+#     if a>b and a>c:
+#         return f'{a} is maximum.'
+#     elif b>a and b>c:
+#         return f"{b} is maximum."
+#     elif c>a and c>b:
+#         return f"{c} is maximum."
+    
+# a = int(input('Enter first number'))
+# b = int(input('Enter second number'))
+# c = int(input('Enter third number'))
+
+# print(maximum(a,b,c))
+
+
+
+
 def maximum(a,b,c):
     if a>b and a>c:
-        return f'{a} is maximum.'
+        return a
     elif b>a and b>c:
-        return f"{b} is maximum."
+        return b
     elif c>a and c>b:
-        return f"{c} is maximum."
+        return c
     
-a = int(input('Enter first number'))
-b = int(input('Enter second number'))
-c = int(input('Enter third number'))
-
-print(maximum(a,b,c))
+print("Maximum of three numbers is", maximum(10,56,8))
