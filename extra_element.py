c82eff08d8a73e6336add1b9687c9fa3796af741
Srashti Srivastava <srivastavasrashti45@gmail.com>
2025-04-04 13:34:30 +0530

    changes

--- extra_element.py
+++ extra_element.py
@@ -1,11 +1,20 @@
+# arr1 = [2,4,6,8,9,10,12]
+# arr2 = [2,4,6,8,10,12]
+# sum1 = 0
+# sum2 = 0 
+# for i in arr1:
+#     sum1 = sum1 + i
+
+# for j in arr2:
+#     sum2 = sum2 + j
+
+# print(sum1 - sum2)
+
+
+
+
 arr1 = [2,4,6,8,9,10,12]
 arr2 = [2,4,6,8,10,12]
-sum1 = 0
-sum2 = 0 
-for i in arr1:
-    sum1 = sum1 + i
-
-for j in arr2:
-    sum2 = sum2 + j
-
-print(sum1 - sum2)
+sum1 = sum(arr1)
+sum2 = sum(arr2)
+print("Extra Element is ",(sum1-sum2))
