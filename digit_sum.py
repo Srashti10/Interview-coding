4812d84bbbc23fe11c43919f0a144bca80d09686
Srashti Srivastava <srivastavasrashti45@gmail.com>
2025-04-04 13:26:54 +0530

    changes

--- digit_sum.py
+++ digit_sum.py
@@ -7,15 +7,12 @@ def digit_sum(num):
 num = int(input('Enter the number: '))
 print(digit_sum(num))
 
-# num = int(input('Enter the number: '))
-# sum = 0
-# for digits in str(num):
-#     sum = sum+int(digits)
+
+
+
+
+# num = int(input("Enter a number:"))
+# sum = 0 
+# for i in str(num):
+#     sum += int(i)
 # print(sum)
-
-
-num = int(input("Enter a number:"))
-sum = 0 
-for i in str(num):
-    sum += int(i)
-print(sum)
