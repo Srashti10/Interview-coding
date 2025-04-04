2f85ea6cb6dc25bdfa25929f6762fb3737319fc0
Srashti Srivastava <srivastavasrashti45@gmail.com>
2025-04-04 14:28:34 +0530

    changes

--- missing_element.py
+++ missing_element.py
@@ -1,10 +1,19 @@
-arr = [1,2,3,5]
+# arr = [1,2,3,5]
+# n = 5
+# sum1 = int((n*(n+1))/2)
+# sum2 = 0
+# for i in arr:
+#     sum2 = sum2 + i
+# print(sum1)
+# print(sum2)
+# m = sum1 - sum2
+# print(m)
+
+
+
+
+arr = [1,2,4,5]
 n = 5
-sum1 = int((n*(n+1))/2)
-sum2 = 0
-for i in arr:
-    sum2 = sum2 + i
-print(sum1)
-print(sum2)
-m = sum1 - sum2
-print(m)
+sum1 = sum(arr)
+sum2 = (n*(n+1))/2
+print("The missing number is",int(sum2-sum1))
