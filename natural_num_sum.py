fe5cc98b5a006404ce0cdc0eb65c5f25025b7995
Srashti Srivastava <srivastavasrashti45@gmail.com>
2025-04-04 14:41:33 +0530

    changes

--- natural_num_sum.py
+++ natural_num_sum.py
@@ -1,3 +1,8 @@
-num = int(input('Enter the number'))
-sum = int((num * (num+1))/2)
-print(f'sum of {num} natural numbers is', sum)
+# num = int(input('Enter the number'))
+# sum = int((num * (num+1))/2)
+# print(f'sum of {num} natural numbers is', sum)
+
+def natural_num_sum(num):
+    sum = (num*(num+1))/2
+    return sum
+print(int(natural_num_sum(10)))
