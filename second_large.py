52cf1838b8ad072a570e5c92a4bf695db5b5ccd8
Srashti Srivastava <srivastavasrashti45@gmail.com>
2025-04-10 17:39:36 +0530

    changes

--- second_large.py
+++ second_large.py
@@ -1,11 +1,6 @@
 # list = [45,78,94,52,82,89]
-# n = len(list)
-# list.sort()
-# print(list)
-# print(list[n-2])
-
-
-list = [45,78,94,52,82,89]
 n = len(list)
 list.sort()
+print(list)
 print(list[n-2])
+
