a5f900af123ceb769adc4a318e605ccd6358e1c3
Srashti Srivastava <srivastavasrashti45@gmail.com>
2025-04-10 13:59:22 +0530

    changes

--- palindrome_num.py
+++ palindrome_num.py
@@ -1,6 +1,37 @@
-num = int(input('Enter the number: '))
-temp = str(num)
-if temp == temp[::-1]:
-    print('Number is palindrome.')
-else:
-    print('Number is not palindrome.')
+# num = int(input('Enter the number: '))
+# temp = str(num)
+# if temp == temp[::-1]:
+#     print('Number is palindrome.')
+# else:
+#     print('Number is not palindrome.')
+
+
+
+
+
+
+
+
+
+
+
+# num = int(input("Enter a number: \n"))
+# temp = str(num)
+# if temp == temp[::-1]:
+#     print("Number is Palindrome.")
+# else:
+#     print("Number is not Palindrome.")
+
+
+
+
+
+
+def palindrome(num):
+    temp = str(num)
+    if temp == temp[::-1]:
+        return "Number is Palindrome."
+    else:
+        return "Number is not Palindrome."
+    
+print(palindrome(23))
