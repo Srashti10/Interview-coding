4420f975a14ba13985b007723bda02f0ad2cda49
Srashti Srivastava <srivastavasrashti45@gmail.com>
2025-04-10 18:43:03 +0530

    changes

--- swap-variable.py
+++ swap-variable.py
@@ -1,32 +1,19 @@
-# a = int(input('Enter the value of a:'))
-# b = int(input('Enter the value of b:'))
-# a,b = b,a
-# print(a,b)
-
-# aa = "Hello"
-# bb = "World"
-# aa,bb = bb,aa
-# print(aa,bb)
-
-
-# a = "10"
-# b = "20"
-# a = a+b
-# b = a-b
-# a = a-b
-# print(a)
-# print(b)
-
-a = "Hello"
-b = "World"
+a = int(input('Enter the value of a:'))
+b = int(input('Enter the value of b:'))
 a,b = b,a
-print(a)
-print(b)
+print(a,b)
+
+aa = "Hello"
+bb = "World"
+aa,bb = bb,aa
+print(aa,bb)
 
 
-a = 10
-b = 20
+a = "10"
+b = "20"
 a = a+b
 b = a-b
 a = a-b
-print(a,b)
+print(a)
+print(b)
+
